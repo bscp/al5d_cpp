1dceeb63bad3275da1ab63d88b6bf9629cc28a5f
Bas <b.cop.nl@gmail.com>
2020-05-03 16:51:44 +0000

    Add assess script

---
+++ scripts/assess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# TODO : find all assesments scripts
+# TODO : run all assessments from a loop
+
+# Run unittests
+# TODO
+
+# Run unittests with Valgrind
+# TODO
+
+# Run Clang-tidy
+# TODO
+
+# Run cppcheck
+scripts/assessments/cppcheck.sh
