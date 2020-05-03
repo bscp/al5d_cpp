4096957de417a89a040289fb18eef4be46748b60
Bas <b.cop.nl@gmail.com>
2020-05-03 14:23:14 +0000

    Add clean build capability to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-# Determine build type
-BUILD_TYPE="Release"
-if [ $# -gt 0 ]; then  # if any argument specified
-    BUILD_TYPE=$1
+# Set default values
+BUILD_METHOD="REBUILD"
+BUILD_TYPE="release"
+
+# Parse specified flags
+# -m stands for method or build_method
+# -t stands for type or build_type
+while getopts m:t: option; do
+    case "${option}" in
+        m) BUILD_METHOD=${OPTARG};;
+        t) BUILD_TYPE=${OPTARG};;
+    esac
+done
+
+# Format the BUILD_METHOD input
+BUILD_METHOD=${BUILD_METHOD,,} # make all letters lowercase
+BUILD_METHOD=${BUILD_METHOD:0:1} # take first letter
+
+# Remove current build directory on clean build
+if [ "$BUILD_METHOD" = "c" ] ; then
+    echo "Cleaning first"
+    rm -rf ./build
 fi
 
 # Make sure the build directory exists and it is the workspace
-mkdir build -p && cd build
+mkdir build -p
 
-# Generate the build files
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-
-# Build the program using the build files
-make -j 8  # with 8 cpu threads
+# Generate the build files and build with 8 threads
+# The parenthesis mean the directory change is only for this command
+(cd build && cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE .. && make -j 8)
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 # Installing basically means moving library files
 # to a directory where the system is able to find it.
 
-# Run the build script
-scripts/build.sh Release
+# Run a 'clean' 'release' build
+scripts/build.sh -m clean_build -t release
 
 # Install the build files
 sudo cmake --build ./build --target install
