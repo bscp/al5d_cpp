777e40cbf88dc09036ced7b47b31cee45ff5eb59
Bas <b.cop.nl@gmail.com>
2020-05-11 11:44:35 +0000

    Remove redundant action

--- main.cpp
+++ main.cpp
@@ -11,11 +11,8 @@ int main()
     auto al5d = al5d::AL5D::from_json_file(file_path);
 
     auto move_duration = al5d::Duration::from_milliseconds(2000);
-    al5d.move_to({{al5d::JOINT_BASE, 90}}, move_duration);
+    al5d.move_to({{al5d::JOINT_BASE, 0}}, move_duration);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
-    move_duration = al5d::Duration::from_milliseconds(2000);
-    al5d.move_to({{al5d::JOINT_SHOULDER, 90}}, move_duration);
     
     al5d.stop();
 }
