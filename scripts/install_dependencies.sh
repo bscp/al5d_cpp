45002e68fa41e93b23b646c79eacd9852297b5d2
Bas <b.cop.nl@gmail.com>
2020-05-13 12:58:09 +0000

    Fix the bug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,4 +4,4 @@
 git clone -q https://github.com/jbeder/yaml-cpp.git /yaml-cpp \
     && mkdir -p /yaml-cpp/build \
     && (cd /yaml-cpp/build && cmake .. -DYAML_BUILD_SHARED_LIBS=ON && make && make install) \
-    && rm -rf /yaml-cpp
+    # && rm -rf /yaml-cpp # https://github.com/jbeder/yaml-cpp/issues/299
