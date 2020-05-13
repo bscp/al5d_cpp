df81b7847b5831a5f4ec5ebe4746e3bd2159f6e9
Bas <b.cop.nl@gmail.com>
2020-05-13 13:51:06 +0000

    Add optional dependency installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,11 +2,23 @@
 # This script builds the project and then moves the result
 # to a system directory. This moving is called installing
 
-# Install dependencies first 
-scripts/install_dependencies.sh
+# Set default values
+INSTALL_DEPENDENCIES=false
 
-# Run a 'clean' 'release' build
+# Parse specified flags
+while getopts d option; do
+    case "${option}" in
+        d) INSTALL_DEPENDENCIES=true;
+    esac
+done
+
+# Install dependencies first 
+if [ "$INSTALL_DEPENDENCIES" = true ] ; then
+    scripts/install_dependencies.sh
+fi
+
+# Run a build with method='clean' type='release'
 scripts/build.sh -m clean_build -t release
 
 # Install the build files
-sudo cmake --build ./build --target install
+cmake --build ./build --target install
